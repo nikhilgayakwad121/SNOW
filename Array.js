1e9a93f1413bf2a095ad968978b26d9263b35c32
Nikhil Gayakwad <118470700+nikhilgayakwad121@users.noreply.github.com>
2025-09-10 10:11:54 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,3 +1,6 @@
+Methods:
+
+
 //Add Elements in the Array
 var arr = [];
 arr.push('item 1');
@@ -7,6 +10,13 @@ console.log(arr[0]);
 console.log('array Started');
 for(var i =0;i<arr.length;i++)
 {
-
     console.log(arr[i]);
 }
+
+//Revered Array by method
+var num_arr = [1,2,3,4,5];
+console.log(arr)
+num_arr.reverse();
+console.log(arr);
+
+//Reversed Array by
